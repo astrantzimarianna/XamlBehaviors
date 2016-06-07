293888c71bfc8899718fc8791df03fbb57aeebe4
Marianna Astrantzi <m.astrantzi@epsilonnet.gr>
2016-06-07 13:52:57 +0300

    xamarin project commit

---
+++ XamlBehaviors/XamlBehaviors/XamlBehaviors/Behaviors/ConfirmPasswordBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamlBehaviors
+{
+    public class ConfirmPasswordBehavior : Behavior<Entry>
+    {
+        static readonly BindablePropertyKey IsValidPropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(ConfirmPasswordBehavior), false);
+        public static readonly BindableProperty IsValidProperty = IsValidPropertyKey.BindableProperty;
+
+        public static readonly BindableProperty CompareToEntryProperty = BindableProperty.Create("CompareToEntry", typeof(Entry), typeof(ConfirmPasswordBehavior), null);
+
+        public Entry CompareToEntry
+        {
+            get { return (Entry)base.GetValue(CompareToEntryProperty); }
+            set { base.SetValue(CompareToEntryProperty, value); }
+        }
+        public bool IsValid
+        {
+            get { return (bool)base.GetValue(IsValidProperty); }
+            private set { base.SetValue(IsValidPropertyKey, value); }
+        }
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += HandleTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= HandleTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+        void HandleTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (CompareToEntry.Text != null)
+            {
+                var password = CompareToEntry.Text;
+                var confirmPassword = e.NewTextValue;
+                IsValid = password.Equals(confirmPassword);
+            }
+        }
+    }
+}
---
+++ XamlBehaviors/XamlBehaviors/XamlBehaviors/Behaviors/PasswordValidatorBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamlBehaviors
+{
+    public class PasswordValidatorBehavior : Behavior<Entry>
+    {
+        const string passwordRegex = @"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$";
+
+        // Creating BindableProperties with Limited write access: http://iosapi.xamarin.com/index.aspx?link=M%3AXamarin.Forms.BindableObject.SetValue(Xamarin.Forms.BindablePropertyKey%2CSystem.Object) 
+
+        static readonly BindablePropertyKey IsValidPropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(Entry), false);
+
+        public static readonly BindableProperty IsValidProperty = IsValidPropertyKey.BindableProperty;
+
+        public bool IsValid
+        {
+            get { return (bool)base.GetValue(IsValidProperty); }
+            private set { base.SetValue(IsValidPropertyKey, value); }
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += HandleTextChanged;
+        }
+
+
+        void HandleTextChanged(object sender, TextChangedEventArgs e)
+        {
+            IsValid = (Regex.IsMatch(e.NewTextValue, passwordRegex, RegexOptions.None, TimeSpan.FromMilliseconds(250)));
+            ((Entry)sender).TextColor = IsValid ? Color.Default : Color.Red;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= HandleTextChanged;
+        }
+    }
+}
